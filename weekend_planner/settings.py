fa1922e476c7aee7e5008d1221fce86bbefca26c
Mathilde Barbier <mathilde.barbier@ppro.com>
2018-09-20 21:50:52 +0200

    Set debug = true to inspect production error

--- weekend_planner/settings.py
+++ weekend_planner/settings.py
@@ -6,11 +6,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+#
+# if DEVELOPMENT:
+#     DEBUG = True
+# else:
+#     DEBUG = False
 
-if DEVELOPMENT:
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
